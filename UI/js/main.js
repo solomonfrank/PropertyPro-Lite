f6a4c2157c1235c417b2b1db7b42f7eabbe2fb43
solomonfrank <solomonfrank73@hotmail.com>
2019-06-20 03:00:11 +0100

    feature(user dashboard): user can upload and edit through the dashboard [Delivers #166784226]

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,4 +1,14 @@
 document.querySelector('.icon').addEventListener('click', () => {
+    let navCont = document.querySelector('.nav-container');
+    let toggleAside = document.querySelector('.toggle-class');
+    let imgWrapper = document.querySelector('.img-wrapper');
+    if (navCont) {
+        navCont.classList.toggle('open');
+    }
 
-    document.querySelector('.nav-container').classList.toggle('open');
+    if (toggleAside) {
+        toggleAside.classList.toggle('open');
+        imgWrapper.classList.toggle('img-open');
+    }
+
 })
