e2d9e17a32f26a7d811af106cfef85cd6cbee2ff
solomonfrank <solomonfrank73@hotmail.com>
2019-06-21 01:03:13 +0100

    feature(delete-advertise-property): agent should be able to delete sold or unwanted property [Delivers #166821685]

--- UI/js/main.js
+++ UI/js/main.js
@@ -11,4 +11,30 @@ document.querySelector('.icon').addEventListener('click', () => {
         imgWrapper.classList.toggle('img-open');
     }
 
-})
+});
+let modalBtn = document.getElementById("btn-del")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+let close = document.querySelector(".close")
+let okBtn = document.querySelector(".ok-btn")
+
+okBtn.onclick = function () {
+    modal.style.display = "none";
+    modalBtn.style.backgroundColor = 'gray';
+
+}
+
+modalBtn.onclick = function () {
+    modal.style.display = "block"
+}
+close.onclick = function () {
+    modal.style.display = "none"
+}
+closeBtn.onclick = function () {
+    modal.style.display = "none"
+}
+window.onclick = function (e) {
+    if (e.target == modal) {
+        modal.style.display = "none"
+    }
+}
