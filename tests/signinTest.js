905d60531ec25f22aea69901e52ba22ee544ce72
solomonfrank <solomonfrank73@hotmail.com>
2019-07-01 22:03:15 +0100

    chore(signin endpoint): [Delivers #167027810]

---
+++ tests/signinTest.js
@@ -0,0 +1,83 @@
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index';
+
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('Testing for signin endpoint', () => {
+    it('User should not sign in a user when email is empty', (done) => {
+        const dataVal = {
+            email: '',
+            password: '123456',
+        };
+        chai
+            .request(app)
+            .post('/api/v1/signin')
+            .send(dataVal)
+            .end((err, res) => {
+                // eslint-disable-next-line no-console
+
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.have
+                    .property('data')
+                    .equal('"email address" is not allowed to be empty');
+                res.body.should.have.property('status').equal(400);
+                done();
+            });
+    });
+
+    it('User should not sign in a user when password is empty', (done) => {
+        const dataVal = {
+            email: 'test6@yahoo.com',
+
+            password: '',
+        };
+        chai
+            .request(app)
+            .post('/api/v1/signin')
+            .send(dataVal)
+            .end((err, res) => {
+                // eslint-disable-next-line no-console
+
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.have
+                    .property('data')
+                    .equal('"password" is not allowed to be empty');
+                res.body.should.have.property('status').equal(400);
+
+                done();
+            });
+    });
+
+    it('sign in user when all fields are provided', (done) => {
+        const dataVal = {
+            email: 'test6@yahoo.com',
+
+            password: '1234567',
+        };
+        chai
+            .request(app)
+            .post('/api/v1/signin')
+            .send(dataVal)
+            .end((err, res) => {
+                // res.body.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('data');
+                res.body.data.should.have.property('id');
+                res.body.data.should.have.property('token');
+                res.body.data.should.have.property('email');
+                res.body.data.should.have.property('firstName');
+                res.body.data.should.have.property('lastName');
+                res.body.data.should.have.property('createdAt');
+                res.body.data.should.have.property('address');
+                res.body.data.should.have.property('gender');
+                res.body.should.have.property('status').equal(200);
+                done();
+            });
+    });
+});
