c21e1126128402868e02c8e53743f9fed5df097a
solomonfrank <solomonfrank73@hotmail.com>
2019-07-12 18:04:18 +0100

    edit signUp test

--- tests/signupTest.js
+++ tests/signupTest.js
@@ -298,7 +298,7 @@ describe('Testing for signup endpoint', () => {
 
     it('sign up user when all fields are provided', (done) => {
         const dataVal = {
-            email: 'test1312@gmail.com',
+            email: 'test131211@gmail.com',
             password: '1234567',
             confirmPassword: '1234567',
             firstName: 'test',
