cb57825807a18da8e4de99dbeef3aa6e4c0d8f44
solomonfrank <solomonfrank73@hotmail.com>
2019-07-15 21:44:58 +0100

    modify user controller

--- controller/PropertyController.js
+++ controller/PropertyController.js
@@ -58,7 +58,7 @@ class PropertyController {
         let searchBy = { id }
 
         try {
-            let found = await User.init().find(searchBy);
+            let found = await User.init().find({ id });
             console.log(found.rows[0].owner_email);
             body.owner_email = found.rows[0].owner_email;
             console.log(body);
