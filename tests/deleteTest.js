68eac8207a6ae6527f5abc4da37c573434867fa8
solomonfrank <solomonfrank73@hotmail.com>
2019-08-04 20:39:04 +0100

    edit config

--- tests/deleteTest.js
+++ tests/deleteTest.js
@@ -7,58 +7,42 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('Testing for delete property endpoint', function () {
-    let tokens;
-    this.timeout(5000);
+  let tokens;
+  this.timeout(5000);
 
 
+  it('user can delete specific property details if valid token provided and property is available', (done) => {
+    // mock login to get token
+    (async function () {
+      const valid_input = {
+        email: 'test10@yahoo.com',
+        password: '1234567',
+      };
+
+      let res;
+
+      try {
+        const request = chai.request(app).keepOpen();
 
 
-    it('user can delete specific property details if valid token provided and property is available', (done) => {
-        //mock login to get token
-        (async function () {
-            const valid_input = {
-                "email": "test10@yahoo.com",
-                "password": "1234567"
-            }
+        const signResponse = await request.post('/auth/signin').send(valid_input);
+        const { token } = signResponse.body.data;
+        const id = 17;
+        const data = {
+          token,
+        };
+        res = await request.delete(`/property/${id}`).set('Authorization', `Bearer ${token}`).send(data);
 
-            let res;
-
-            try {
-                let request = chai.request(app).keepOpen();
+        res.body.should.have.status('error');
 
 
-                let signResponse = await request.post('/auth/signin').send(valid_input);
-                let token = signResponse.body.data.token;
-                const id = 17;
-                const data = {
-                    token,
-                };
-                res = await request.delete(`/property/${id}`).set('Authorization', `Bearer ${token}`).send(data);
+        done();
+      } catch (err) {
+        //  console.log(err.stack)
+        res.body.should.have.status('success');
+      }
+    }());
 
-                res.body.should.have.status('error');
-
-
-
-                done()
-            } catch (err) {
-                //  console.log(err.stack)
-                res.body.should.have.status('success')
-            }
-
-        })();
-
-        //send login request to the app to receive token
-
-
-
-
-
-
-
-
-
-    });
-
-
-
+    // send login request to the app to receive token
+  });
 });
--- tests/signupTest.js
+++ tests/signupTest.js
@@ -9,144 +9,144 @@ chai.use(chaiHttp);
 // eslint-disable-next-line prettier/prettier
 describe('Testing for signup endpoint', () => {
 
-    it('user should not signup when email is empty', (done) => {
-        const dataVal = {
-            email: '',
-            password: '1234567',
+  it('user should not signup when email is empty', (done) => {
+    const dataVal = {
+      email: '',
+      password: '1234567',
 
-            first_name: 'test',
-            last_name: 'est',
+      first_name: 'test',
+      last_name: 'est',
 
-            address: '5 hillary street',
-            phone_number: '7363737376',
+      address: '5 hillary street',
+      phone_number: '7363737376',
 
 
-        };
-        chai.request(app).post('/auth/signup').send(dataVal).end((err, res) => {
-            res.should.have.status(400);
-            res.body.should.be.an('object');
-            res.body.should.have.property('error').equal('email address is invalid');
-            res.body.should.have.property('status').equal('error');
+    };
+    chai.request(app).post('/auth/signup').send(dataVal).end((err, res) => {
+      res.should.have.status(400);
+      res.body.should.be.an('object');
+      res.body.should.have.property('error').equal('email address is invalid');
+      res.body.should.have.property('status').equal('error');
 
-            done();
-        });
+      done();
     });
+  });
 
-    it('user should not signup when email is wrong', (done) => {
-        const dataVal = {
-            email: 'yuueueu.com',
-            password: '1234567',
+  it('user should not signup when email is wrong', (done) => {
+    const dataVal = {
+      email: 'yuueueu.com',
+      password: '1234567',
 
-            first_name: 'test',
-            last_name: 'est',
+      first_name: 'test',
+      last_name: 'est',
 
-            address: '5 hillary street',
-            phone_number: '7363737376',
+      address: '5 hillary street',
+      phone_number: '7363737376',
 
 
-        };
-        chai.request(app).post('/auth/signup').send(dataVal).end((err, res) => {
-            res.should.have.status(400);
-            res.body.should.be.an('object');
-            res.body.should.have.property('error').equal('email address is invalid');
-            res.body.should.have.property('status').equal('error');
+    };
+    chai.request(app).post('/auth/signup').send(dataVal).end((err, res) => {
+      res.should.have.status(400);
+      res.body.should.be.an('object');
+      res.body.should.have.property('error').equal('email address is invalid');
+      res.body.should.have.property('status').equal('error');
 
-            done();
-        });
+      done();
     });
+  });
 
-    it('User should not sign up a user when password is not provided', (done) => {
-        const dataVal = {
-            email: 'test5915@yahoo.com',
-            password: '',
+  it('User should not sign up a user when password is not provided', (done) => {
+    const dataVal = {
+      email: 'test5915@yahoo.com',
+      password: '',
 
-            first_name: 'test',
-            last_name: 'est',
+      first_name: 'test',
+      last_name: 'est',
 
-            address: '5 hillary street',
-            phone_number: '7363737376',
-
-
-        };
-        chai
-            .request(app)
-            .post('/auth/signup')
-            .send(dataVal)
-            .end((err, res) => {
-                // eslint-disable-next-line no-console
-
-                res.should.have.status(400);
-                res.body.should.be.an('object');
-                res.body.should.have.property('error').equal('some fields are required');
-                res.body.should.have.property('status').equal('error');
-                done();
-            });
-    });
-
-    it('User should not sign up a user if email already exist', (done) => {
-        const dataVal = {
-            email: 'test5@yahoo.com',
-            password: '1234567',
-
-            first_name: 'test',
-            last_name: 'test',
-
-            address: '5 hillary street',
-            phone_number: '7363737376',
+      address: '5 hillary street',
+      phone_number: '7363737376',
 
 
-        };
-        chai
-            .request(app)
-            .post('/auth/signup')
-            .send(dataVal)
-            .end((err, res) => {
-                // eslint-disable-next-line no-console
+    };
+    chai
+      .request(app)
+      .post('/auth/signup')
+      .send(dataVal)
+      .end((err, res) => {
+        // eslint-disable-next-line no-console
 
-                res.should.have.status(400);
-                res.body.should.be.an('object');
-                res.body.should.have.property('error').equal('email already exist');
-                res.body.should.have.property('status').equal('error');
-                done();
-            });
-    });
+        res.should.have.status(400);
+        res.body.should.be.an('object');
+        res.body.should.have.property('error').equal('some fields are required');
+        res.body.should.have.property('status').equal('error');
+        done();
+      });
+  });
 
+  it('User should not sign up a user if email already exist', (done) => {
+    const dataVal = {
+      email: 'test5@yahoo.com',
+      password: '1234567',
 
-    it('sign up user when all fields are provided', (done) => {
-        const dataVal = {
+      first_name: 'test',
+      last_name: 'test',
 
-            email: 'test000051133@gmail.com',
-
-            password: '1234567',
-
-            first_name: 'test',
-            last_name: 'test',
-
-            address: '5 hillary street',
-            phone_number: '7363737376',
+      address: '5 hillary street',
+      phone_number: '7363737376',
 
 
-        };
+    };
+    chai
+      .request(app)
+      .post('/auth/signup')
+      .send(dataVal)
+      .end((err, res) => {
+        // eslint-disable-next-line no-console
+
+        res.should.have.status(400);
+        res.body.should.be.an('object');
+        res.body.should.have.property('error').equal('email already exist');
+        res.body.should.have.property('status').equal('error');
+        done();
+      });
+  });
 
 
-        chai
-            .request(app)
-            .post('/auth/signup')
-            .send(dataVal)
-            .end((err, res) => {
-                res.body.should.have.status('success');
-                res.body.should.be.a('object');
-                res.body.should.have.property('data');
-                res.body.data.should.have.property('id');
-                res.body.data.should.have.property('token');
-                res.body.data.should.have.property('email');
-                res.body.data.should.have.property('first_name');
-                res.body.data.should.have.property('last_name');
-                res.body.data.should.have.property('address');
-                res.body.data.should.have.property('phone_number');
-                res.body.data.should.have.property('created_at');
+  it('sign up user when all fields are provided', (done) => {
+    const dataVal = {
 
-                done();
-            });
-    });
+      email: 'test000051133@gmail.com',
+
+      password: '1234567',
+
+      first_name: 'test',
+      last_name: 'test',
+
+      address: '5 hillary street',
+      phone_number: '7363737376',
+
+
+    };
+
+
+    chai
+      .request(app)
+      .post('/auth/signup')
+      .send(dataVal)
+      .end((err, res) => {
+        res.body.should.have.status('success');
+        res.body.should.be.a('object');
+        res.body.should.have.property('data');
+        res.body.data.should.have.property('id');
+        res.body.data.should.have.property('token');
+        res.body.data.should.have.property('email');
+        res.body.data.should.have.property('first_name');
+        res.body.data.should.have.property('last_name');
+        res.body.data.should.have.property('address');
+        res.body.data.should.have.property('phone_number');
+        res.body.data.should.have.property('created_at');
+
+        done();
+      });
+  });
 });
